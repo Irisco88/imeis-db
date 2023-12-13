f643e05f3243e5f31a80e82cd1be29ac8a106210
irisco88 <iris1988noori@gmail.com>
2023-12-13 12:16:04 +0330

    fix bug

--- migrations/sqls/20230714103109_init.sql
+++ migrations/sqls/20230714103109_init.sql
@@ -2,15 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE imeis
 (
-    id SERYAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     imeis_name VARCHAR(255) NOT NULL,
     device_name  VARCHAR(255),
     owner_id   INTEGER      NOT NULL DEFAULT 0,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE UNIQUE INDEX idx_imeis_unique_imeis_nam ON imeis (imeis_name);
-CREATE UNIQUE INDEX idx_imeis_unique_device_name ON imeis (device_name);
+CREATE UNIQUE INDEX idx_imeiss_unique_imeis_name ON imeis (imeis_name);
+CREATE UNIQUE INDEX idx_imeiss_unique_device_name ON imeis (device_name);
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- +goose StatementEnd
